688e1a34a88fa7c255407228b9268430570a38e5
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-06-16 17:17:27 -0300

    Fix: Bug of showing avatar in tweets fixed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import chalk from 'chalk';
 
 let users = [];
 let tweets = [];
+let userAvatar;
 
 const app = express();
 app.use(cors());
@@ -11,23 +11,33 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
 
     res.status(200).send('teste');
 });
 
 app.post("/sign-up", (req, res) => {
     users.push(req.body);
+    userAvatar = req.body.avatar
     res.status(200).send(console.log('Tudo ok!', users));
 });
 
-app.post("/tweets", (req, res) => {
-    tweets.push(req.body)
+app.post("/tweets", (req, res) => { 
+    const data = req.body;
+
+	const tweetData = {
+		username: data.username,
+		avatar: userAvatar,
+		tweet: data.tweet
+	}
+
+    tweets.push(tweetData)
     res.status(200).send(tweets);
 });
 
-app.get("/tweets", (req, res) => {
+app.get("/tweets", (_, res) => {
     const slicedTweets = tweets.slice(0, 10);
+
     res.status(200).send(slicedTweets);
 });
 
